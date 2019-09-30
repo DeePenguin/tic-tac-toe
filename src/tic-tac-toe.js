06704cb5fca09e71e7ee09c47a601966c5bc1a09
DeePenguin <_deadpenguin_@mail.ru>
2019-10-01 01:57:47 +0300

    feat: add implementation

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,56 @@
 class TicTacToe {
-    constructor() {
+  constructor() {
+    this.currentSymbol = 'x';
+    this.nextSymbol = 'o';
+    this.desc = Array(3).fill(null).map(() => Array(3).fill(null));
+    this.finished = false;
+    this.winner = null;
+  }
+  
+  getCurrentPlayerSymbol() {
+    return this.currentSymbol;
+  }
 
+  nextTurn(row, col) {
+    if (!this.desc[row][col]) {
+      this.desc[row][col] = this.currentSymbol;
+      [this.currentSymbol, this.nextSymbol] = [this.nextSymbol, this.currentSymbol];
     }
+  }
 
-    getCurrentPlayerSymbol() {
+  isFinished() {
+    return !!this.getWinner() || this.noMoreTurns();
+  }
 
+  getWinner() {
+  if ((this.desc[0][0] === this.desc[1][1] && this.desc[1][1] === this.desc[2][2]) || 
+    (this.desc[0][2] === this.desc[1][1] && this.desc[1][1] === this.desc[2][0])) this.winner = this.desc[1][1];
+  else {
+    for (let i = 0; i < 3; i++) {
+      if (this.desc[i][0] !== null && this.desc[i][0] === this.desc[i][1] && this.desc[i][1] === this.desc[i][2]) {
+        this.winner = this.desc[i][0];
+        break;
+      } else if (this.desc[0][i] !== null && this.desc[0][i] === this.desc[1][i] && this.desc[2][i] === this.desc[0][i]) {
+        this.winner = this.desc[0][i];
+        break;
+      }
     }
+  }
 
-    nextTurn(rowIndex, columnIndex) {
+    return this.winner;
+  }
 
-    }
+  noMoreTurns() {
+    return this.desc.every(row => row.every(col => col));
+  }
 
-    isFinished() {
+  isDraw() {
+    return !(!this.isFinished() || !!this.getWinner());
+  }
 
-    }
-
-    getWinner() {
-
-    }
-
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  getFieldValue(row, col) {
+    return this.desc[row][col];
+  }
 }
 
 module.exports = TicTacToe;
